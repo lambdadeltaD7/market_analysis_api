d6e46f61078d147ad4e42179d787029fd78f1c0c
Lolikion228 <st117139@student.spbu.ru>
2026-07-30 13:29:07 +0300

    changes in main_app/Dockerfile

--- main_app/db.py
+++ main_app/db.py
@@ -1,12 +1,16 @@
 import os
 from sqlalchemy import create_engine
 
-# user = os.environ.get("POSTGRES_USER")
+user = os.environ.get("POSTGRES_USER")
+passw = os.environ.get("POSTGRES_PASSWORD")
+db_name = os.environ.get("POSTGRES_DB")
+db_host = os.environ.get("POSTGRES_HOST")
+db_port = os.environ.get("POSTGRES_PORT")
 
-user = "abobus"
-passw = "qwerty"
-db_host = "127.0.0.1"
-db_name = "sh_db"
-db_port = 8002 
+# user = "abobus"
+# passw = "qwerty"
+# db_host = "127.0.0.1"
+# db_name = "sh_db"
+# db_port = 8002 
 
 sql_engine = create_engine(f"postgresql+psycopg://{user}:{passw}@{db_host}:{db_port}/{db_name}")
